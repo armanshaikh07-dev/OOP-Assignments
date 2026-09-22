69bd7db9279a9f6c3ae8c15e738e9c69d78fa494
Arman Shaikh <armanshaikh7867862@gmail.com>
2026-09-22 19:53:54 +0530

    Update Assignment-2.cpp

--- Assignment-2.cpp
+++ Assignment-2.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+public:
+    int rollNumber;
+    string name;
+    string department;
+    float cgpa;
+
+    void input()
+    {
+        cout << "Enter roll number: ";
+        cin >> rollNumber;
+
+        cout << "Enter name: ";
+        cin >> name;
+
+        cout << "Enter department: ";
+        cin >> department;
+
+        cout << "Enter CGPA: ";
+        cin >> cgpa;
+    }
+
+    void display()
+    {
+        cout << "\n--- Student Details ---" << endl;
+        cout << "Roll Number: " << rollNumber << endl;
+        cout << "Name: " << name << endl;
+        cout << "Department: " << department << endl;
+        cout << "CGPA: " << cgpa << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+    s.input();
+    s.display();
+    return 0;
+}
 
